7970b60de8b63f5329f8f9123a0d67e4e878ecbb
simran <simranmanwani0@gmail.com>
2024-07-02 13:38:25 +0530

    upadted email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
         <div className='border-b border-neutral-900 pb-20'>
             <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='my-10 text-center text-4xl'>Get in Touch</motion.h2>
             <div className="text-center tracking-tighter">
-                <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="my-4">{CONTACT.Email}</motion.p>
-
+                <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="my-4">{CONTACT.Email1}</motion.p>
+                <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="my-4">{CONTACT.Email2}</motion.p>
             </div>
         </div>
     )
